583a24a8c7685de8bd2243e83d607fbda420a98b
saideep <saideepjonnalagadda1@gmail.com>
2024-09-14 09:57:57 -0500

    changed lr

--- LinearRegression.py
+++ LinearRegression.py
@@ -18,37 +18,24 @@ class LinearRegression:
         best_weights = self.w
         best_loss = float('inf')
         patience_cnt = 0
-
-        # Training loop
         for epoch in range(max_epochs):
-            # Shuffle training data
             ind = np.arange(X_train.shape[0])
             np.random.shuffle(ind)
             X_train_shuffled = X_train[ind]
             y_train_shuffled = y_train[ind]
-
             #Gradient Descent
             for start in range(0, X_train.shape[0], batch_size):
                 end = start + batch_size
                 X_b = X_train_shuffled[start:end]
                 y_b = y_train_shuffled[start:end]
-                
-                # Make predictions
                 y_pred = X_b @ self.weights
-                
-                # Compute errors
-                errors = y_pred - y_b
-
-                # Compute gradients
+                errors = y_pred - y_
                 grad_weights = X_b.T @ errors / batch_size + regularization * self.w / batch_size
-                
-                # Update weights
                 self.w -= 0.01 * grad_weights
 
-            # Validate
             test_pred = X_test @ self.w
             test_loss = np.mean((test_pred - y_test) ** 2)
-            self.loss.append(test_loss)  # Track validation loss
+            self.loss.append(test_loss)
 
             if test_loss < best_loss:
                 best_loss = test_loss
@@ -63,7 +50,7 @@ class LinearRegression:
         self.w = best_weights
 
     def predict(self, X):
-        X = np.hstack([X, np.ones((X.shape[0], 1))])  # Add bias term column
+        X = np.hstack([X, np.ones((X.shape[0], 1))])
         return X @ self.w
 
     def score(self, X, y):
@@ -71,8 +58,8 @@ class LinearRegression:
         return np.mean((y_pred - y.reshape(-1, y.shape[1] if y.ndim > 1 else 1)) ** 2)
 
     def save(self, filepath):
-        np.savez(filepath, weights=self.weights)
+        np.savez(filepath, weights=self.w)
 
     def load(self, filepath):
         npzfile = np.load(filepath)
-        self.weights = npzfile['weights']
+        self.w = npzfile['weights']
